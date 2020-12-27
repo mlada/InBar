f76cc7d1e71ecd811ef704ae4c6d08ad6f6b7903
mlada <mlada.pechorina@gmail.com>
2020-12-27 20:11:03 +0700

    add login

---
+++ InBar/ViewModels/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace InBar.ViewModels
+{
+    public class LoginViewModel : BaseViewModel 
+    {
+
+        public LoginViewModel()
+        {
+            Title = "Login";
+        }
+
+        public ICommand NextCommand { get; }
+        public Entry password { get; }
+        public Entry login { get; }
+
+    }
+}
---
+++ InBar/Views/LoginPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace InBar.Views
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
